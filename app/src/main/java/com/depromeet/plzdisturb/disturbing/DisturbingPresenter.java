5a7cf1c69263fed15189108c63fc7a8ebd2542c7
jonghyo <dlwhdgy9190@gmail.com>
2019-11-23 18:07:45 +0900

    presenter 생성

---
+++ app/src/main/java/com/depromeet/plzdisturb/disturbing/DisturbingPresenter.java
@@ -0,0 +1,22 @@
+package com.depromeet.plzdisturb.disturbing;
+
+public class DisturbingPresenter implements DisturbingContract.Presenter {
+
+    public DisturbingPresenter() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void disturb(int userId) {
+
+    }
+
+    @Override
+    public void free(int userId) {
+
+    }
+}
